36e9177f367a36ce4aeeea27fe77ae789ce6f8b6
Tanvi Sanjay Palkar <102821962+Tanvi-Palkar@users.noreply.github.com>
2022-07-10 20:44:46 +0530

    Update tuple_len.py

--- Practical No-7/tuple_len.py
+++ Practical No-7/tuple_len.py
@@ -1,9 +1,8 @@
 #program too find lenght of tuple
 
-tuple1=[2,6,32,76,22,5,11]
+tuple1=(2,6,32,76,22,5,11)
 count =0
 n = len(tuple1)
-for i in range(0,n):
-    count = count+1
+
 print("Given tuple :",tuple1)
-print("Lenght of tuple:",count)
+print("Lenght of tuple:",n)
