8467bbe42e3a4663730edd102fdb6c2a93132f7a
Tanvi Sanjay Palkar <102821962+Tanvi-Palkar@users.noreply.github.com>
2022-07-13 00:13:24 +0530

    Update sort_tuple.py

--- Practical No-7/sort_tuple.py
+++ Practical No-7/sort_tuple.py
@@ -1,12 +1,11 @@
 #program to sort tuple by total digits
 
-
-
-    
+def count_d(tup):
+    return (sum([len(str(ele)) for ele in tup ]))
 list = [(11, 23, 45, 34), (34, 67), (323,), (78, 15, 73, 95), (60, 35)]
-
 print("The list is : ")
 print(list)
-result = sorted(list, key = lambda tup : sum([len(str(ele)) for ele in tup ]))
-print("The sorted tuples are ")
-print(result)
+list.sort(key = count_d)
+
+print("The List after sorted tuples are ")
+print(list)
